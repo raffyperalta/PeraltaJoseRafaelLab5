b79e940535031ecf1f606cc7824e42aa9af04233
Raffy Peralta <raffy.peralta.01@gmail.com>
2018-10-08 17:12:30 +0800

    Initial commit

---
+++ app/src/main/java/joserafael/peralta/com/peraltajoserafaellab5/Service.java
@@ -0,0 +1,18 @@
+package joserafael.peralta.com.peraltajoserafaellab5;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.support.annotation.Nullable;
+import android.util.Log;
+
+public class Service extends IntentService {
+
+    public Service(){
+        super("Service");
+    }
+
+    @Override
+    protected void onHandleIntent(@Nullable Intent intent) {
+        Log.d("4ITF", "Service is running in the background!");
+    }
+}
